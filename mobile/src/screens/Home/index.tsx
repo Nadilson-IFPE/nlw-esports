8a2461a066ac57d19578727bd70ed9068ab295b5
Nadilson <nadilson@protonmail.com>
2022-09-13 02:52:13 -0300

    Rocketseat NLW eSports - Aula prática 2/6 - High speed

---
+++ mobile/src/screens/Home/index.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { Image, View, FlatList } from "react-native";
+
+import logoImg from "../../assets/logo-nlw-esports.png";
+import { GameCard } from "../../components/GameCard";
+import { Heading } from "../../components/Heading";
+
+import { styles } from "./styles";
+import { GAMES } from "./../../utils/games";
+
+export function Home() {
+  return (
+    <View style={styles.container}>
+      <Image source={logoImg} style={styles.logo} />
+
+      <Heading
+        title="Encontre seu duo!"
+        subTitle="Selecione o game que deseja jogar..."
+      />
+
+      <FlatList
+        data={GAMES}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => <GameCard data={item} />}
+        showsHorizontalScrollIndicator={false}
+        horizontal
+        contentContainerStyle={styles.contentList}
+      />
+    </View>
+  );
+}
